40c2659a4455135343c1f04520c93c203a384266
Simon Podlipsky <simon@podlipsky.net>
2019-10-09 17:18:58 +0200

    Drop nullable type hints from callbacks in AssertHelper

--- src/Type/BeberleiAssert/AssertHelper.php
+++ src/Type/BeberleiAssert/AssertHelper.php
@@ -222,37 +222,37 @@ class AssertHelper
 	{
 		if (self::$resolvers === null) {
 			self::$resolvers = [
-				'integer' => function (Scope $scope, Arg $value): ?\PhpParser\Node\Expr {
+				'integer' => function (Scope $scope, Arg $value): \PhpParser\Node\Expr {
 					return new \PhpParser\Node\Expr\FuncCall(
 						new \PhpParser\Node\Name('is_int'),
 						[$value]
 					);
 				},
-				'string' => function (Scope $scope, Arg $value): ?\PhpParser\Node\Expr {
+				'string' => function (Scope $scope, Arg $value): \PhpParser\Node\Expr {
 					return new \PhpParser\Node\Expr\FuncCall(
 						new \PhpParser\Node\Name('is_string'),
 						[$value]
 					);
 				},
-				'float' => function (Scope $scope, Arg $value): ?\PhpParser\Node\Expr {
+				'float' => function (Scope $scope, Arg $value): \PhpParser\Node\Expr {
 					return new \PhpParser\Node\Expr\FuncCall(
 						new \PhpParser\Node\Name('is_float'),
 						[$value]
 					);
 				},
-				'numeric' => function (Scope $scope, Arg $value): ?\PhpParser\Node\Expr {
+				'numeric' => function (Scope $scope, Arg $value): \PhpParser\Node\Expr {
 					return new \PhpParser\Node\Expr\FuncCall(
 						new \PhpParser\Node\Name('is_numeric'),
 						[$value]
 					);
 				},
-				'boolean' => function (Scope $scope, Arg $value): ?\PhpParser\Node\Expr {
+				'boolean' => function (Scope $scope, Arg $value): \PhpParser\Node\Expr {
 					return new \PhpParser\Node\Expr\FuncCall(
 						new \PhpParser\Node\Name('is_bool'),
 						[$value]
 					);
 				},
-				'scalar' => function (Scope $scope, Arg $value): ?\PhpParser\Node\Expr {
+				'scalar' => function (Scope $scope, Arg $value): \PhpParser\Node\Expr {
 					return new \PhpParser\Node\Expr\FuncCall(
 						new \PhpParser\Node\Name('is_scalar'),
 						[$value]
@@ -269,19 +269,19 @@ class AssertHelper
 						[$value]
 					);
 				},
-				'isResource' => function (Scope $scope, Arg $value): ?\PhpParser\Node\Expr {
+				'isResource' => function (Scope $scope, Arg $value): \PhpParser\Node\Expr {
 					return new \PhpParser\Node\Expr\FuncCall(
 						new \PhpParser\Node\Name('is_resource'),
 						[$value]
 					);
 				},
-				'isCallable' => function (Scope $scope, Arg $value): ?\PhpParser\Node\Expr {
+				'isCallable' => function (Scope $scope, Arg $value): \PhpParser\Node\Expr {
 					return new \PhpParser\Node\Expr\FuncCall(
 						new \PhpParser\Node\Name('is_callable'),
 						[$value]
 					);
 				},
-				'isArray' => function (Scope $scope, Arg $value): ?\PhpParser\Node\Expr {
+				'isArray' => function (Scope $scope, Arg $value): \PhpParser\Node\Expr {
 					return new \PhpParser\Node\Expr\FuncCall(
 						new \PhpParser\Node\Name('is_array'),
 						[$value]
@@ -311,43 +311,43 @@ class AssertHelper
 						)
 					);
 				},
-				'true' => function (Scope $scope, Arg $expr): ?\PhpParser\Node\Expr {
+				'true' => function (Scope $scope, Arg $expr): \PhpParser\Node\Expr {
 					return new \PhpParser\Node\Expr\BinaryOp\Identical(
 						$expr->value,
 						new \PhpParser\Node\Expr\ConstFetch(new \PhpParser\Node\Name('true'))
 					);
 				},
-				'false' => function (Scope $scope, Arg $expr): ?\PhpParser\Node\Expr {
+				'false' => function (Scope $scope, Arg $expr): \PhpParser\Node\Expr {
 					return new \PhpParser\Node\Expr\BinaryOp\Identical(
 						$expr->value,
 						new \PhpParser\Node\Expr\ConstFetch(new \PhpParser\Node\Name('false'))
 					);
 				},
-				'null' => function (Scope $scope, Arg $expr): ?\PhpParser\Node\Expr {
+				'null' => function (Scope $scope, Arg $expr): \PhpParser\Node\Expr {
 					return new \PhpParser\Node\Expr\BinaryOp\Identical(
 						$expr->value,
 						new \PhpParser\Node\Expr\ConstFetch(new \PhpParser\Node\Name('null'))
 					);
 				},
-				'notNull' => function (Scope $scope, Arg $expr): ?\PhpParser\Node\Expr {
+				'notNull' => function (Scope $scope, Arg $expr): \PhpParser\Node\Expr {
 					return new \PhpParser\Node\Expr\BinaryOp\NotIdentical(
 						$expr->value,
 						new \PhpParser\Node\Expr\ConstFetch(new \PhpParser\Node\Name('null'))
 					);
 				},
-				'same' => function (Scope $scope, Arg $value1, Arg $value2): ?\PhpParser\Node\Expr {
+				'same' => function (Scope $scope, Arg $value1, Arg $value2): \PhpParser\Node\Expr {
 					return new \PhpParser\Node\Expr\BinaryOp\Identical(
 						$value1->value,
 						$value2->value
 					);
 				},
-				'notSame' => function (Scope $scope, Arg $value1, Arg $value2): ?\PhpParser\Node\Expr {
+				'notSame' => function (Scope $scope, Arg $value1, Arg $value2): \PhpParser\Node\Expr {
 					return new \PhpParser\Node\Expr\BinaryOp\NotIdentical(
 						$value1->value,
 						$value2->value
 					);
 				},
-				'subclassOf' => function (Scope $scope, Arg $expr, Arg $class): ?\PhpParser\Node\Expr {
+				'subclassOf' => function (Scope $scope, Arg $expr, Arg $class): \PhpParser\Node\Expr {
 					return new \PhpParser\Node\Expr\FuncCall(
 						new \PhpParser\Node\Name('is_subclass_of'),
 						[
